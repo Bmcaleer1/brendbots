102f6b8d39508ec37e083f8483ec6a1b68f377ba
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 14:26:41 -0500

    added the floor

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 import time
 import pybullet as p
+import pybullet_data
 
 
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1 / 60)
