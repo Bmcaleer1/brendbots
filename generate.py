edc596a581ac242bf0abaa201ce4464117555d39
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 15:55:18 -0500

    Create_World

--- generate.py
+++ generate.py
@@ -1,12 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("worlds.sdf")
-start = 1
-sizes = [start * (0.9 ** i) for i in range(10)]
 
-for i in range(10):
-    for x in range(-2, 3):
-        for y in range(-2, 3):
-            z = sizes[i] / 2
-            pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
-pyrosim.End()
+
+def Create_World():
+    """
+    Create an SDF file named 'world.sdf' containing a single cube.
+    """
+    pyrosim.Start_SDF("world.sdf")
+
+    pyrosim.Send_Cube(
+        name="Box",
+        pos=[-4, 4, 0.5],
+        size=[1, 1, 1]
+    )
+
+    pyrosim.End()
