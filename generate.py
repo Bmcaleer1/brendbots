458d41dd055d7247a337f346f34f3b09268e2bff
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 14:46:02 -0500

    ground level start

--- generate.py
+++ generate.py
@@ -1,7 +1,15 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+#variable initialization
+#size
+length = 1
+width = 2
+height = 3
+#position
+x = 0
+y = 1.5
+z = 1.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length , width, height])
 
 pyrosim.End()
