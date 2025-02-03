6768bcb28d43f916965210655f5d018da585f727
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 15:00:18 -0500

    2 links

--- generate.py
+++ generate.py
@@ -4,12 +4,17 @@ pyrosim.Start_SDF("box.sdf")
 #variable initialization
 #size
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 #position
 x = 0
-y = 1.5
-z = 1.5
+y = 0.5
+z = 0.5
+#position 2
+x2=1
+y2=0.5
+z2=1.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length , width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length , width, height])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -5,9 +5,13 @@ import pybullet_data
 
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.setGravity(0, 0, -9.8)
+
 planeID = p.loadURDF("plane.urdf")
+
 p.loadSDF("box.sdf")
+
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1 / 60)
