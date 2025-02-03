ebf5bb4565635a6c78892148e43e35ccd29d21da
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 15:03:28 -0500

    tower simulation complete

--- generate.py
+++ generate.py
@@ -1,20 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-#variable initialization
-#size
-length = 1
-width = 1
-height = 1
-#position
-x = 0
-y = 0.5
-z = 0.5
-#position 2
-x2=1
-y2=0.5
-z2=1.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length , width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length , width, height])
+start = 1
+sizes = [start * (0.9 ** i) for i in range(10)]
 
+for i in range(10):
+    for x in range(-2, 3):
+        for y in range(-2, 3):
+            z = sizes[i] / 2
+            pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
 pyrosim.End()
