d36308abb41859f5f3901b38d5bbf4cde7b32a19
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 22:10:38 -0500

    finished joints

--- generate.py
+++ generate.py
@@ -6,22 +6,33 @@ def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(
         name="Box",
-        pos=[0, 0, 0.5],  # Position at the origin
+        pos=[-3, 2, 0.5],  # Position at the origin
         size=[1, 1, 1]    # Size of the cube
     )
     pyrosim.End()
+
+
 def Create_Robot():
-    """
-    Create a URDF file named 'body.urdf' containing a simple robot.
-    """
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(
-        name="Torso",
-        pos=[0, 0, 1.5],  # Position of the Torso
-        size=[1, 1, 1]    # Size of the Torso
-    )
+
+    # Main body segment
+    pyrosim.Send_Cube(name="Base", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+
+    # Joint connecting the base to the rear limb
+    pyrosim.Send_Joint(name="Base_RearJoint", parent="Base", child="RearLeg", type="revolute", position=[1, 0, 1])
+
+    # Rear limb segment
+    pyrosim.Send_Cube(name="RearLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+
+    # Joint linking the base to the front limb
+    pyrosim.Send_Joint(name="Base_FrontJoint", parent="Base", child="FrontLeg", type="revolute", position=[2, 0, 1])
+
+    # Front limb segment
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+
+    # Finalize and save the robot structure
     pyrosim.End()
 
 
-Create_World()
 Create_Robot()
+Create_World()
