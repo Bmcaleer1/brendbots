d64ee951699b2276017afc84f47fa34dfc310137
bmcaleer1 <brendancmcaleer@gmail.com>
2025-02-03 21:12:36 -0500

    two boxes stacked on top of eachother

--- generate.py
+++ generate.py
@@ -3,15 +3,25 @@ import pyrosim.pyrosim as pyrosim
 
 
 def Create_World():
-    """
-    Create an SDF file named 'world.sdf' containing a single cube.
-    """
     pyrosim.Start_SDF("world.sdf")
-
     pyrosim.Send_Cube(
         name="Box",
-        pos=[-4, 4, 0.5],
-        size=[1, 1, 1]
+        pos=[0, 0, 0.5],  # Position at the origin
+        size=[1, 1, 1]    # Size of the cube
     )
-
     pyrosim.End()
+def Create_Robot():
+    """
+    Create a URDF file named 'body.urdf' containing a simple robot.
+    """
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(
+        name="Torso",
+        pos=[0, 0, 1.5],  # Position of the Torso
+        size=[1, 1, 1]    # Size of the Torso
+    )
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -10,10 +10,15 @@ p.setGravity(0, 0, -9.8)
 
 planeID = p.loadURDF("plane.urdf")
 
-p.loadSDF("worlds.sdf")
+# Load the world SDF file
+worldID = p.loadSDF("world.sdf")
+
+# Load the robot URDF file
+robotID = p.loadURDF("body.urdf")
 
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1 / 60)
     print(f"Iteration {i}")
+
 p.disconnect()
